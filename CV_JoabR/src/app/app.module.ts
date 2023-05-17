f2341fbcaa6e917850490fb34b5cc37e434a4b7f
Joab Ramos <joab.ramos27@gmail.com>
2023-05-17 13:47:55 -0600

    agregue los contenedores a angular principales como article, aside, footer, header, main, nav cambie algunos estilos quite margenes

--- CV_JoabR/src/app/app.module.ts
+++ CV_JoabR/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { SectionComponent } from './section/section.component';
+import { ArticleComponent } from './article/article.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    SectionComponent,
+    ArticleComponent,
+    AsideComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
